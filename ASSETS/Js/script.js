25d9fb31ddfe21e45db663b1752ecbe063dae832
Bobmanuel Pamela <info.automantia@gmail.com>
2023-10-22 00:32:37 -0700

    first commit

---
+++ ASSETS/Js/script.js
@@ -0,0 +1,113 @@
+// var age = 23
+
+// if (age == 18){
+//     console.log("you are allowed");
+//     } else if (age == 20) {
+//      console.log( "you are allowed");
+        
+//     }else if (age == 22)  {
+//     console.log( "you are allowed");
+//     }else {
+//     console.log("you are not allowed")
+//     }
+
+
+    // var age = 30
+
+    // if(age = 30){
+
+    //     console.log("30 years is not allowed")
+    //     }
+        
+
+
+        // loop
+
+        // for(starting; Condition; increment){
+
+
+        // }
+
+        // for(var i=0; i<100; i+=4){
+        //     console.log("running")
+
+        // }
+
+        // // for even numbers
+        // for(var i=0; i<20; i+=2){
+        //     console.log(i)
+
+        // }
+
+        // assignment variable that can output prime numbers from 1 to 100
+ //  ARRAY$
+        // var names = ["blessing" , "gift" , "emmanuel"]
+        // for(var i = 0; i<3; i++){
+        //     console.log(names[i])
+        // }
+
+
+       
+        // var names = ["blessing" , "gift" , "emmanuel"]
+        // for(var i = 0; i<21; i++){
+        //     console.log(names[i])
+        // }
+
+        // OBJECT$ to create object use curly bracket , it stores key value things like css coour and stuff
+
+        // var person = { 
+        //     age:27 , 
+        //     hair: "black" , 
+        //     likes: ["food", "movies", "dance"], 
+        //     hobbies:{football:"once a day", movies:"once a week"}
+        // }
+
+
+
+        // console.log(person.age)
+        //     console.log(person.age, 
+        //     person.hair)
+        //     person.hair = "brown"
+
+        // console.log(person.likes[1])
+        // console.log(person.hobbies.movies)
+
+        // FUNCTION$$
+
+        function sum() {
+            var x = 8;
+            var y = 10;
+            var sum = x + y;
+            console.log(sum)
+        }
+        sum()
+
+        function sum(x,y) {
+           
+            var sum = x + y;
+            console.log(sum)
+        }
+        sum(50,9)
+        sum(12,11)
+
+        function sayname(name) {
+            console.log("my name is " + name)
+        }
+
+        sayname("emmanuel")
+        sayname("JOHN")
+        sayname("emmanuel")
+        sayname("emmanuel")
+
+
+// RETURN STatEMENT
+
+function sum(x,y) {
+           
+    var sum = x + y;
+    console.log(sum)
+    return 100;
+}
+var addition = sum(50, 9)
+
+
